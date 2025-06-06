3ebc755f12f361422f96cda8eddda99ef7d06258
Justin Ishiguro <ishigurj@amazon.com>
2025-06-06 14:50:16 -0700

    fix: support disconnected metric graphs

--- JSInjections/clickMaxGraphPoint.js
+++ JSInjections/clickMaxGraphPoint.js
@@ -8,7 +8,7 @@ function clickMaxGraphPoint(chartPosition, checkboxPosition) {
   const EVENT_LAYER_SELECTOR = ".event-layer";
 
   const DATA_POINT_SELECTOR = "circle.datapoint";
-  const ALL_DATA_POINT_SELECTOR = "circle.all-datapoint";
+  const ALL_DATA_POINT_SELECTOR = "circle.all-datapoint:not(.hidden)";
 
   const MAX_RETRIES = 10;
   const RETRY_DELAY = 500;
