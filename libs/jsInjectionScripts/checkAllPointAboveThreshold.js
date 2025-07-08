984063d8818276b4b40d14c798ef232363f1ee46
Justin Ishiguro <ishigurj@amazon.com>
2025-07-08 09:32:19 -0700

    fix: added javadoc comments to JS injections

--- libs/jsInjectionScripts/checkAllPointAboveThreshold.js
+++ libs/jsInjectionScripts/checkAllPointAboveThreshold.js
@@ -1,13 +1,22 @@
+/**
+ * Determines if all data points in the selected metric graph are above a threshold line
+ * or are all zero (based on the provided parameter - checkZero).
+ *
+ * @param {number} chartPosition - Index of the metric graph to be selected.
+ * @param {number} checkboxPosition - Index of the legend checkbox to de-select to clearly display the correct line.
+ * @param {boolean} checkZero - If true, ensures all points are zero (cy = 90); if false, checks if all points are above the threshold line.
+ *
+ * @returns {Promise<boolean>} - Returns true if all data points satisfy the condition, or false if any fail.
+ */
 async function checkAllPointAboveThreshold(
   chartPosition,
   checkboxPosition,
   checkZero
 ) {
+  // Define constants
   const LEADER_BOARD_DATA_POINT_SELECTOR = "circle.leaderboard-datapoint";
   const EVENT_LAYER_SELECTOR = ".event-layer";
-
   const ALL_DATA_POINT_SELECTOR = "circle.all-datapoint";
-
   const IFRAME_SELECTOR = "iframe#microConsole-Pulse";
   const LEGEND_CHECKBOX_SELECTOR = "rect.legend-checkbox";
   const CWDB_CHART_SELECTOR = "div.cwdb-chart";
@@ -18,16 +27,14 @@ async function checkAllPointAboveThreshold(
 
   // Get the iFrame
   const iframe = document.querySelector(IFRAME_SELECTOR);
-
   const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
 
   // Each graph has 2+ lines graphed. To ensure the correct line is selected, we remove the other plot
   const checkboxes = iframeDoc.querySelectorAll(LEGEND_CHECKBOX_SELECTOR);
-
-  const checkbox = checkboxes[checkboxPosition]; // TEST PARAM (3)
+  const checkbox = checkboxes[checkboxPosition];
   const checkboxGroup = checkbox.closest("g.legend.dimmable");
 
-  // Each graph has 2+ lines graphed. To ensure the correct line is selected, we remove the other plot
+  // If this checkbox is not already disabled, we disable it
   if (!checkboxGroup?.classList.contains("legend-disabled")) {
     const checkboxBounds = checkbox.getBoundingClientRect();
     const checkboxHoverX = checkboxBounds.left + checkboxBounds.width / 2;
@@ -54,9 +61,13 @@ async function checkAllPointAboveThreshold(
     checkbox.dispatchEvent(checkboxClickEvent);
   }
 
+  // Query all of the graphs in the iFrame
   const charts = iframeDoc.querySelectorAll(CWDB_CHART_SELECTOR);
 
-  const chart = charts[chartPosition]; // TEST PARAM (2)
+  // Select the specific chart
+  const chart = charts[chartPosition];
+
+  // Get the <rect> element within this chart. This will help us hover over the dynamic part
   const eventLayer = chart.querySelector(EVENT_LAYER_SELECTOR);
 
   // Get the position of this object relative to the viewport (https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect)
@@ -103,9 +114,16 @@ async function checkAllPointAboveThreshold(
   });
   leaderBoardDataPoint.dispatchEvent(leaderBoardDataPointEvent);
 
+  // Get the threshold line
   const annotationLines = iframeDoc.querySelectorAll(ANNOTATION_LINE_SELECTOR);
   const annotationLineY = parseFloat(annotationLines[2].getAttribute("y1"));
 
+  /**
+   * Waits for the chart to populate all data points, then verifies
+   * each point against the threshold or zero value depending on checkZero boolean.
+   *
+   * @returns {Promise<boolean|[]>} - Returns true if all are valid, false if any fail, [] if none found
+   */
   async function waitForAllDatapoints() {
     for (let i = 0; i < MAX_RETRIES; i++) {
       const allDatapoints = iframeDoc.querySelectorAll(ALL_DATA_POINT_SELECTOR);
@@ -113,10 +131,12 @@ async function checkAllPointAboveThreshold(
       if (allDatapoints.length > 0) {
         for (const datapoint of allDatapoints) {
           const cy = parseFloat(datapoint.getAttribute("cy"));
+          // If the points are above the threshold line OR not equal to zero, we immediately return false
           if ((cy > annotationLineY && !checkZero) || (cy != 90 && checkZero)) {
             return false;
           }
         }
+        // If we make it to the end, we return true
         return true;
       }
       await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY));
